6df6b44e0e5a8b1219069af54cd1f00d526e5da9
Anton Shokut <anton.shokut.edu@gmail.com>
2021-06-15 11:33:36 -0700

    Add play install referrer lib

--- App.tsx
+++ App.tsx
@@ -5,9 +5,12 @@ import {
   Text,
   View,
 } from 'react-native';
+import { PlayInstallReferrer } from 'react-native-play-install-referrer';
 
 const App = () => {
-  const [link, setLink] = useState<string|null>(null);
+  const [link, setLink] = useState<string | null>(null);
+  const [referrer, setReferrer] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
 
  useEffect(() => {
   const getUrlAsync = async () => {
@@ -18,11 +21,39 @@ const App = () => {
   getUrlAsync();
  }, [setLink])
 
+ useEffect(() => {
+  PlayInstallReferrer.getInstallReferrerInfo((installReferrerInfo: any, error: any) => {
+    if(error) {
+      setError(error); 
+    } else {
+      setReferrer(installReferrerInfo);
+    }
+  });
+ }, [])
+
   return (
     <SafeAreaView>
       <View>
         <Text>{link || 'No Link'}</Text>
       </View>
+      { referrer && (
+        <View>
+          <Text>{"Install referrer = " + referrer.installReferrer}</Text>
+          <Text>{"Referrer click timestamp seconds = " + referrer.referrerClickTimestampSeconds}</Text>
+          <Text>{"Install begin timestamp seconds = " + referrer.installBeginTimestampSeconds}</Text>
+          <Text>{"Referrer click timestamp server seconds = " + referrer.referrerClickTimestampServerSeconds}</Text>
+          <Text>{"Install begin timestamp server seconds = " + referrer.installBeginTimestampServerSeconds}</Text>
+          <Text>{"Install version = " + referrer.installVersion}</Text>
+          <Text>{"Google Play instant = " + referrer.googlePlayInstant}</Text>
+        </View>
+      )}
+      { error && (
+        <View>
+          <Text>{"Failed to get install referrer info!"}</Text>
+          <Text>{"Response code: " + error.responseCode}</Text>
+          <Text>{"Message: " + error.message}</Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
