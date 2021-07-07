73ca6b0353eca3ac97716e3c91afefd7eac88727
Anton Shokut <anton.shokut.edu@gmail.com>
2021-07-07 13:11:44 -0700

    Adjust regex in pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,12 +3,12 @@
 # Create an .env from AppCenter Environment variables for use with react-native-config
 echo AS-test-root-pre-build
 
-ENV_WHITELIST=${ENV_WHITELIST:-".*"}
+ENV_WHITELIST=${ENV_WHITELIST:-"USER[-|_]DEFINED_.*"}
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n" $ENV_WHITELIST
 
-set | egrep -e $ENV_WHITELIST > .env
+set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env.tmp
+sed -E -e 's/USER-DEFINED_\USER_DEFINED-//g' .env.tmp > env
 
 printf "\n.env created with contents:\n\n"
-
 cat .env
