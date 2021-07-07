ab84df9147f6928bfec7ed790f599f0a1518ebc1
Anton Shokut <anton.shokut.edu@gmail.com>
2021-07-07 12:18:51 -0700

    Add post clone build

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Create an .env from AppCenter Environment variables for use with react-native-config
+echo AS-test
+printf "Creating an .env file with the following whitelist:\n"
+printf "%s\n" $ENV_WHITELIST
+
+set | egrep -e $ENV_WHITELIST > .env
+
+printf "\n.env created with contents:\n\n"
+
+cat .env
