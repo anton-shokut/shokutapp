1d777c22fadcff5b4ffa709a1266fcc050a1a571
Anton Shokut <anton.shokut.edu@gmail.com>
2021-07-07 12:37:42 -0700

    Modufy post-clone build script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Create an .env from AppCenter Environment variables for use with react-native-config
-echo AS-test
+echo AS-test-root
+
+ENV_WHITELIST=${ENV_WHITELIST:-"$.*"}
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n" $ENV_WHITELIST
 
