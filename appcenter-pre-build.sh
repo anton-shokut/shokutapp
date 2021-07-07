f6c3bf0fc45847c53e73e38d1f28c92cb7cf211c
Anton Shokut <anton.shokut.edu@gmail.com>
2021-07-07 14:07:03 -0700

    Print all envs

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,7 +3,10 @@
 # Create an .env from AppCenter Environment variables for use with react-native-config
 echo AS-test-root-pre-build
 
-ENV_WHITELIST=${ENV_WHITELIST:-".*"}
+echo Prin all env
+printenv
+
+ENV_WHITELIST=${ENV_WHITELIST:-"*"}
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n" $ENV_WHITELIST
 
